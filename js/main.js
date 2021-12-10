f36e1da883e1594a37876359173d2a87a64bf11a
Krishna Singh <76508179+King-Krishna@users.noreply.github.com>
2021-12-10 11:46:20 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function secondlayer(){
+    $(this).remove()
+    // $('le').html('<label id='labelEmail' for='inputEmail' class='sr-only after'>Email address :</label>')
+    // $('ie').html('<input type='email' id='inputEmail' class='mt-3 form-control after' placeholder='Email address' autofocus required autocomplete='off'>')
+    // $('ie').html('<label id='labelPassword' for='inputPassword' class='mt-2 sr-only after'>Password :</label>')
+    // $('ie').html('<input type='password' id='inputPassword' class='mt-3 form-control after' placeholder='Password' required>')
+    // $('p').html('<button class='mt-3 btn btn-lg btn btn-outline-primary btn-block' type='button' id='btn-back'>Back</button>');
+    // $('#inputName').css('opacity','0')
+    // $('#inputdob').css('opacity','0')
+    // $('#labelname').css('opacity','0')
+    // $('#labeldob').css('opacity','0')
+    // $('#labelEmail').css('opacity','100')
+    // $('#labelPassword').css('opacity','100')
+    // $('#labelEmail').fadeIn(3000);
+    // $('#labelPassword').fadeIn(3000);
+    // $('#inputEmail').css('opacity','100')
+    // $('#inputPassword').css('opacity','100')
+    // $('#inputEmail').fadeIn(3000);
+    // $('#inputPassword').fadeIn(3000);
+    // $(this).remove();
+    // $('h6').html('<button class='mt-3 btn btn-lg btn btn-outline-primary btn-block' type='submit' id='btn-submit'>Submit</button>');
+    return false; 
+}
